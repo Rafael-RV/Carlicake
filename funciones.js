6afd9c25bdfc3cc343a62f584ed3e66f9fe64175
Rafael Rincón <126123044+Rafaelgitub@users.noreply.github.com>
2023-04-07 09:28:09 -0400

    initial commit

---
+++ funciones.js
@@ -0,0 +1,11 @@
+alert("Bienvenido a Carlicake")
+
+const Cheesecake = 25000;
+const TortaTresLeches = 20000;
+const VolcanDeChocolate = 7000;
+const resultadoBooleano = Cheesecake >= VolcanDeChocolate && TortaTresLeches > VolcanDeChocolate ;
+console.log(resultadoBooleano)
+
+
+
+
